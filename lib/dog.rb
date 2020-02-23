602fc7f1a09d618925460af4b96a235265e8400c
Anastasia Orlova <metkusblackus@gmail.com>
2020-02-23 00:14:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
   def name=(name)
-    @name = name
+    @name = name 
   end
   
   def name
     @name
   end
+
+  def bark=("woof!")
+    @bark="woof!"
+  end
+
+  def bark
+    @bark
+  end
 end
+
